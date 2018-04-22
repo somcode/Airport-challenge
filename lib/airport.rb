7039928a117657e1fb7d25af04528250da042dd0
Somayeh <mail.somayeh@gmail.com>
2018-04-22 18:59:34 +0100

    prevent landing when the weather is stormy

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,20 +4,22 @@ require_relative 'weather'
 class Airport
 
   attr_reader :weather
+  attr_accessor :hangar
 
   def initialize
-    @hanger = []
+    @hangar = []
     @weather = Weather.new
   end
 
   def land(plane)
-    @hanger.push(plane)
+    raise ("You can't land in stormy weather") if @weather.stormy?
+    @hangar.push(plane)
   end
 
   def take_off(plane)
-    raise ("you can't take off in stormy weather") if @weather.stormy?
-    @hanger.delete(plane)
-    @hanger
+    raise ("You can't take off in stormy weather") if @weather.stormy?
+    @hangar.delete(plane)
+    @hangar
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -8,16 +8,27 @@ describe Airport do
 
 
   describe "#land" do
-    it "instruct plane to land at airport" do
-      expect(airport.land(plane1)).to eq ([plane1])
+    context "when the weather is sunny" do
+      it "instruct plane to land at airport" do
+        allow(airport.weather).to receive(:stormy?) { false }
+        expect(airport.land(plane1)).to eq ([plane1])
+      end
     end
+
+    context "when the weather is stormy" do
+      it "prevent landing" do
+        allow(airport.weather).to receive(:stormy?) { true }
+        expect{ airport.land(plane1) }.to raise_error "You can't land in stormy weather"
+      end
+    end
+
   end
 
 
   describe "#take_off" do
     context "when there is one plane at hanger" do
       it "instruct the plane to take off" do
-        airport.land(plane1)
+        airport.hangar = [plane1]
         allow(airport.weather).to receive(:stormy?) { false }
         expect(airport.take_off(plane1)).to eq ([])
       end
@@ -25,8 +36,7 @@ describe Airport do
 
     context "when there are more than one plane in hanger" do
       it "instruct one of the planes in hanger to take off and it confirms plane has taken off" do
-        airport.land(plane1)
-        airport.land(plane2)
+        airport.hangar = [plane1, plane2]
         allow(airport.weather).to receive(:stormy?) { false }
         expect(airport.take_off(plane1)).to eq ([plane2])
       end
@@ -34,9 +44,9 @@ describe Airport do
 
     context "when the weather is stormy" do
       it "prevent the plane from take off" do
-        airport.land(plane1)
+        airport.hangar = [plane1]
         allow(airport.weather).to receive(:stormy?) { true }
-        expect{ airport.take_off(plane1) }.to raise_error "you can't take off in stormy weather"
+        expect{ airport.take_off(plane1) }.to raise_error "You can't take off in stormy weather"
       end
     end
 
